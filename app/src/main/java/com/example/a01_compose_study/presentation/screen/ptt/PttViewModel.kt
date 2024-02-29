9d10f064f01ec170e8b432c95fb8e8d614d903eb
alex-b-min <alex.b.min@selvas.com>
2024-02-29 09:50:44 +0900

    feat: 중복 변수 수

--- app/src/main/java/com/example/a01_compose_study/presentation/screen/ptt/PttViewModel.kt
+++ app/src/main/java/com/example/a01_compose_study/presentation/screen/ptt/PttViewModel.kt
@@ -19,8 +19,6 @@ class PttViewModel @Inject constructor(
 
     private val _domainUiState = UiState._domainUiState
     val announceString = pttManager.announceString
-
-    val announceString = MutableStateFlow("")
     val vrConfig = MutableStateFlow(VrConfig())
     val guideString = MutableLiveData<String>()
 
