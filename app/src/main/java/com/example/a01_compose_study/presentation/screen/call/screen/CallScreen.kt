aa71252c8003e9bababced236653d96dc746313b
alex-b-min <alex.b.min@selvas.com>
2024-03-04 10:18:48 +0900

    refactor: 불필요한 코드 삭제

--- app/src/main/java/com/example/a01_compose_study/presentation/screen/call/screen/CallScreen.kt
+++ app/src/main/java/com/example/a01_compose_study/presentation/screen/call/screen/CallScreen.kt
@@ -2,29 +2,20 @@ package com.example.a01_compose_study.presentation.screen.call.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a01_compose_study.R
 import com.example.a01_compose_study.data.custom.call.fetchAllContacts
 import com.example.a01_compose_study.domain.model.HelpItemData
@@ -32,19 +23,17 @@ import com.example.a01_compose_study.domain.model.ScreenType
 import com.example.a01_compose_study.domain.model.SealedDomainType
 import com.example.a01_compose_study.domain.util.ScreenSizeType
 import com.example.a01_compose_study.presentation.screen.call.CallBusinessEvent
-import com.example.a01_compose_study.presentation.screen.call.CallEvent
 import com.example.a01_compose_study.presentation.screen.call.CallViewModel
 import com.example.a01_compose_study.presentation.screen.main.DomainUiState
 import com.example.a01_compose_study.presentation.screen.main.route.VRUiState
 
 @Composable
 fun CallScreen(
-    callViewModel: CallViewModel = hiltViewModel(),
+    callViewModel: CallViewModel = viewModel(),
     domainUiState: DomainUiState.CallWindow,
     vrUiState: VRUiState,
     vrDynamicBackground: Color,
     fixedBackground: Color,
-//    onChangeScrollIndex: () -> Unit
 ) {
     if (domainUiState.screenType is ScreenType.CallList) {
         Box(modifier = Modifier.fillMaxSize()) {
@@ -55,19 +44,8 @@ fun CallScreen(
                 fixedBackground = fixedBackground,
                 onDismiss = { /*TODO*/ },
                 onBackButton = { /*TODO*/ },
-                onChangeScrollIndex = {
-                    /**
-                     * 이렇게 domainUiState의 scrollIndex의 값을 넣어주고 하위뷰인 CallListWindow()에서 사용해줘야함
-                     * 하지만 현재 domainUiState의 scrollIndex의 값을 변경 시켜주는 이
-                     */
-//                    domainUiState.scrollIndex?.let {
-//                        CallEvent.ChangeScrollIndexEvent(
-//                            it
-//                        )
-//                    }?.let { callViewModel.onCallEvent(it) }
-
-                },
-                onCalling = { callViewModel.onCallBusinessEvent(event = CallBusinessEvent.Calling)
+                onCalling = {
+                    callViewModel.onCallBusinessEvent(event = CallBusinessEvent.Calling)
                 },
                 onItemClick = { /*TODO*/ }
             )
@@ -85,7 +63,6 @@ fun CallListWindow(
     fixedBackground: Color,
     onDismiss: () -> Unit,
     onBackButton: () -> Unit,
-    onChangeScrollIndex: () -> Unit,
     onCalling: () -> Unit,
     onItemClick: (HelpItemData) -> Unit,
 ) {
@@ -111,7 +88,8 @@ fun CallListWindow(
                     }
             )
 
-            CallList(contactList = contactList,
+            CallList(
+                contactList = contactList,
                 vrDynamicBackground = vrDynamicBackground,
                 selectedIndex = selectedIndex,
                 fixedBackground = fixedBackground,
@@ -137,7 +115,6 @@ fun CallListWindowPreview() {
         fixedBackground = Color.Black,
         onDismiss = {},
         onBackButton = {},
-        onChangeScrollIndex = {},
         onCalling = { },
         onItemClick = {}
     )
